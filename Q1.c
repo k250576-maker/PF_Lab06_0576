3384fd5f70d9f00a9237cd3eaf0d2944425d24bf
Anas <k250576@nu.edu.pk>
2025-10-02 12:29:08 +0500

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int n,i;
+   printf("Enter the last number:");
+   scanf("%d",&n);
+   for(i=n;i>=1;i--){
+       printf("%d",i);
+       printf("\n");
+   }
+   
+    return 0;
+}
